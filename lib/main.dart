ca427f11b6f4dd7a5a552cfcbb9b7d7dc234a56a
fierychicken65 <fadiltutu@gmail.com>
2024-09-29 22:40:44 +0530

    added AppCheck(debug)

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:cooker/pages/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cooker/firebase_options.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
   );
   runApp(const MyApp());
 }
