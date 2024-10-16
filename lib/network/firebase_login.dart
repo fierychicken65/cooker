87ea9572eb2b85c46325328b9ad77cb355fcca79
fierychicken65 <fadiltutu@gmail.com>
2024-10-16 12:05:03 +0530

    added firebase vertex AI

--- lib/network/firebase_login.dart
+++ lib/network/firebase_login.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_vertexai/firebase_vertexai.dart';
 
 final FirebaseAuth auth = FirebaseAuth.instance;
 final storage = FirebaseStorage.instance;
+final model =
+    FirebaseVertexAI.instance.generativeModel(model: 'gemini-1.5-flash');
